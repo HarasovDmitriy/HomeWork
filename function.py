8c305c74660c4e3e39cf676aed3851b9ebc3bea9
DmitriyHarasov <dmitryharasov@yandex.ru>
2022-08-11 13:33:27 +0300

    Внесенны комментарии

--- function.py
+++ function.py
@@ -1,32 +1,29 @@
+#Импортируем библиотеки и класс
 import json
 from Class import Candidate
 
-#которая загрузит данные из файла
-
-
-
 def load_candidates(filename):
+    '''Загрузка кандидатов из файла'''
     with open('candidates.json', encoding='UTF-8') as file:
         data = json.load(file)
     return data
 
-#которая покажет всех кандидатов
 def get_all(data):
+    '''Показать всех кондидатов'''
     arr = []
     for item in data:
         candidate = Candidate(item['pk'], item['name'], item['position'], item['skills'].lower(), item['picture'])
         arr.append(candidate)
     return arr
 
-#которая вернет кандидата по pk
 def get_by_pk(pk, arr):
+    '''Показать кондидата по его номеру'''
     for item in arr:
         if item.pk == pk:
             return item
 
-
-#которая вернет кандидатов по навыку
 def get_by_skill(skill_name, data):
+    '''Показать кондидатов по их навыку'''
     arr = []
     for item in data:
         if skill_name in item.skills:
--- main.py
+++ main.py
@@ -1,15 +1,16 @@
+# Импортируем нужные библиотеку\функции
 from flask import Flask
-
 from function import get_all, load_candidates, get_by_pk, get_by_skill
 
+#загружаем список кондидатов
 FILENAME = 'candidates.json'
 data = get_all(load_candidates(FILENAME))
 
+#Странички
 app = Flask(__name__)
-
-
 @app.route('/')
 def index():
+    '''Стартовая страница'''
     str = "<pre>"
     for i in data:
         str += f'{i} \n \n'
@@ -18,6 +19,7 @@ def index():
 
 @app.route('/candidates/<int:pk>')
 def get_user(pk):
+    '''Показывает информацию о кондидате по номеру'''
     user = get_by_pk(pk, data)
     if user:
         str = f'<img src = "{user.picture}">'
@@ -29,6 +31,7 @@ def get_user(pk):
 
 @app.route('/skills/<x>')
 def get_users(x):
+    '''Показывает кондидатов по навыкам'''
     users = get_by_skill(x, data)
     if users:
         str = '<pre>'
@@ -39,7 +42,6 @@ def get_users(x):
         str = 'NOT FOUND'
     return str
 
-
-
+#Запускаем сайт на порту 5000
 if __name__ == '__main__':
     app.run(port=5000)
